dd292d000ae9eb012c8507df40b3d84cb2aa6632
mostofa-40 <mostofamaruf836@gmail.com>
2026-05-11 12:44:07 +0600

    Create sum_on_segment.cpp

---
+++ CF_EDU/Prefix_Sum/sum_on_segment.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+
+vector<ll> findPrefixSums(vector<ll>& a){
+    int n = a.size();
+    vector<ll> prefixSums(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        prefixSums[i + 1] = prefixSums[i] + a[i];
+    }
+    return prefixSums;
+}
+
+ll getSum(int left, int right, vector<ll>& b){ // [left, right)
+    return b[right] - b[left];
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    for (int tc = 1; tc <= t; tc++){
+        int n; 
+        cin >> n;
+        vector<ll> a(n), p(n + 1, 0);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        p = findPrefixSums(a);
+        int q; 
+        cin >> q;
+        while (q--){
+            int l, r; cin >> l >> r;
+            cout << getSum(l - 1, r, p) << '\n';
+        }
+    }
+
+    return 0;
+}
